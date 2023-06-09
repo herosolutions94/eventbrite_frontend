70efb9bd40a52ef17caf0a148d7e0c4a3470bd21
Tipu <30400782+tipu07@users.noreply.github.com>
2023-06-09 16:45:50 +0500

    Transactions

--- pages/organizer/transactions/withdrawBlock.tsx
+++ pages/organizer/transactions/withdrawBlock.tsx
@@ -1,7 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import style from "@/styles/scss/app.module.scss"
 
 const WithdrawBlock = () => {
+	const [withdraw, setWithdraw] = useState(false)
+	const withdrawHandle = () => {
+		setWithdraw(!withdraw)
+	}
 	return (
 		<>
 			<div className={style.balance_blk}>
@@ -11,10 +15,62 @@ const WithdrawBlock = () => {
 				<div className={style.price}>
 					Current Balance: <span className="price">£40.0</span>
 				</div>
-				<button type="button" className={`${style.site_btn} ${style.sm}`}>
+				<button type="button" className={`${style.site_btn} ${style.sm}`} onClick={withdrawHandle}>
 					Withdraw Funds
 				</button>
 			</div>
+			{withdraw ? (
+				<div className={style.popup}>
+					<div className={style.table_dv}>
+						<div className={style.table_cell}>
+							<div className={style.contain}>
+								<div className="row justify-content-center">
+									<div className="col-lg-6 col-sm-7 col-12">
+										<div className={style._inner}>
+											<button type="button" className={style.x_btn} onClick={withdrawHandle}></button>
+											<h5 className="mb-4">Add Payment method</h5>
+											<form action="" method="post">
+												<div className={style.withdraw_block}>
+													<div className={style.lbl_btn}>
+														<input type="radio" name="payment" id="bank" value="bank-account" checked={true} />
+														<label htmlFor="bank">Bank Account</label>
+													</div>
+													<div className={style.inside_blk}>
+														<h6>Bank Account</h6>
+														<div className={style.form_blk}>
+															<select name="" id="" className={style.input}>
+																<option value="">Select</option>
+																<option value="">Wells Fargo Checking Account</option>
+																<option value="">SunTrust Checking Account</option>
+															</select>
+														</div>
+													</div>
+													<hr />
+													<div className={style.lbl_btn}>
+														<input type="radio" name="payment" id="paypal" value="paypal" />
+														<label htmlFor="paypal">Paypal</label>
+													</div>
+													<div className={style.inside_blk}>
+														<h6>PayPal Address</h6>
+														<div className={style.form_blk}>
+															<input type="email" name="" id="" className={style.input} placeholder="eg: spfr2013qz7@nomail.com" />
+														</div>
+													</div>
+												</div>
+												<div className={`${style.btn_blk} mt-5 justify-content-center`}>
+													<button type="submit" className={style.site_btn}>
+														Submit
+													</button>
+												</div>
+											</form>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			) : null}
 		</>
 	)
 }
