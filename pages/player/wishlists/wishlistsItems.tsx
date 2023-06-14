4926449375ddaeaa0268f23851b9e842ff57ea5f
ammardevdksa <ammar.vtechnocrat@gmail.com>
2023-06-14 16:10:01 +0500

    update

--- pages/player/wishlists/wishlistsItems.tsx
+++ pages/player/wishlists/wishlistsItems.tsx
@@ -1,99 +1,46 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import style from "@/styles/scss/app.module.scss"
 import Pagination from "@/components/pagination"
 import CategoryCard from "@/components/categoryCard"
 import { PhotoBlog01, PhotoBlog02, PhotoBlog03, PhotoMainSlide } from "@/components/images"
+import axios from "axios"
+import Cookies from "js-cookie"
+import { toast } from "react-toastify"
 
-const SEARCH_RESULTS = [
-	{
-		id: 1,
-		title: "White Keep Assault",
-		link: "?",
-		wishlist: true,
-		tag: "esports",
-		date: "September 14, 2021",
-		text: "Maecenas tempus, tellus eget rhoncus, sem quam tempus, tellus eget rhoncus vel velit auctor aliquet",
-		img: PhotoBlog01,
-	},
-	{
-		id: 2,
-		title: "Dota 2 Tournament",
-		link: "?",
-		wishlist: true,
-		tag: "esports",
-		date: "July 07, 2021",
-		text: "Maecenas tempus, tellus eget rhoncus, sem quam tempus, tellus eget rhoncus vel velit auctor aliquet",
-		img: PhotoBlog02,
-	},
-	{
-		id: 3,
-		title: "Winners on ESL Pro",
-		link: "?",
-		wishlist: true,
-		tag: "esports",
-		date: "January 31, 2021",
-		text: "Maecenas tempus, tellus eget rhoncus, sem quam tempus, tellus eget rhoncus vel velit auctor aliquet",
-		img: PhotoBlog03,
-	},
-	{
-		id: 4,
-		title: "Keep Winner DOTA",
-		link: "?",
-		wishlist: true,
-		tag: "esports",
-		date: "September 14, 2021",
-		text: "Maecenas tempus, tellus eget rhoncus, sem quam tempus, tellus eget rhoncus vel velit auctor aliquet",
-		img: PhotoMainSlide,
-	},
-	{
-		id: 5,
-		title: "White Keep Assault",
-		link: "?",
-		wishlist: true,
-		tag: "esports",
-		date: "September 14, 2021",
-		text: "Maecenas tempus, tellus eget rhoncus, sem quam tempus, tellus eget rhoncus vel velit auctor aliquet",
-		img: PhotoBlog01,
-	},
-	{
-		id: 6,
-		title: "Dota 2 Tournament",
-		link: "?",
-		wishlist: true,
-		tag: "esports",
-		date: "July 07, 2021",
-		text: "Maecenas tempus, tellus eget rhoncus, sem quam tempus, tellus eget rhoncus vel velit auctor aliquet",
-		img: PhotoBlog02,
-	},
-	{
-		id: 7,
-		title: "Winners on ESL Pro",
-		link: "?",
-		wishlist: true,
-		tag: "esports",
-		date: "January 31, 2021",
-		text: "Maecenas tempus, tellus eget rhoncus, sem quam tempus, tellus eget rhoncus vel velit auctor aliquet",
-		img: PhotoBlog03,
-	},
-	{
-		id: 8,
-		title: "Keep Winner DOTA",
-		link: "?",
-		wishlist: true,
-		tag: "esports",
-		date: "September 14, 2021",
-		text: "Maecenas tempus, tellus eget rhoncus, sem quam tempus, tellus eget rhoncus vel velit auctor aliquet",
-		img: PhotoMainSlide,
-	},
-]
 const WishlistsItems = () => {
+	const [wishlistsItems, setWishlistsItems] = useState<any>([]);
+	useEffect(() => {
+		fetchWishlistsItems();
+	}, []);
+	
+	const fetchWishlistsItems = async () => {
+		try {
+			const response = await axios.post(`${process.env.API_URL}/wishlist`, {
+				user_id: Cookies.get("user_id"),	
+			});
+			if (response.status === 200) {
+				setWishlistsItems(response.data.data);
+			}
+		} catch (error : any) {
+			toast.error(error.response.data.message);
+		}
+	};
 	return (
 		<>
 			<div className="row">
-				{SEARCH_RESULTS.map((data) => {
+				{wishlistsItems.length > 0 && 
+					wishlistsItems.map((data : any) => {
 					return (
 						<div className="col-lg-4" key={data.id}>
-							<CategoryCard {...data} />
+							<CategoryCard 
+								tournamentId={data.id}
+								title={data.title}
+								link={`/production/tournament-detail?id=` + data.id}
+								tag={data?.category?.name}
+								date={data.start_date}
+								text={'lorem ipsum'}
+								img={process.env.ASSET_URL + data?.images[0]?.image}
+							/>
 						</div>
 					)
 				})}
