35cca9dc68a1bdc38d0b747b07f1c217ce814209
Muqadsa Riaz <imac@192.168.1.18>
2024-03-01 18:54:52 +0500

    chnge

--- pages/profile-description/sellerdetails.tsx
+++ pages/profile-description/sellerdetails.tsx
@@ -35,8 +35,8 @@ export default function Sellerdetail() {
                 <h3>Over View</h3>
                 <p>
                   Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
+                  typesetting industry. Lorem Ipsum has been the industrys
+                  standard dummy text ever since the 1500s when an unknown
                   printer took a galley of type and scrambled it to make a type
                   specimen book.
                 </p>
